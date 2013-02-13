c11a78e69d3bafa779b7272d2a86c818073a30ad
Geoff Schorkopf <gschorkopf@gmail.com>
2013-02-13 14:57:59 -0700

    Copy and pasted information from inv, inv items, and customer to their builder files.

--- test/customer_builder_test.rb
+++ test/customer_builder_test.rb
@@ -2,8 +2,17 @@ require './test/support'
 
 class CustomerBuilderTest < MiniTest::Unit::TestCase
   
-  def test_it_exists
+  def setup
+    @output = CustomerBuilder.load_customers
+  end
 
+  def test_it_exists
+    cust_builder = CustomerBuilder.new
+    assert_kind_of CustomerBuilder, cust_builder
+  end
+
+  def test_it_builds_customers_from_a_csv
+    assert_operator 5, :<=, @output.size
   end
 
 end
