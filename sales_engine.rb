0b36e6c73f1c6b403c5554695bc7b1888e6c701b
Geoff Schorkopf <gschorkopf@gmail.com>
2013-02-12 18:47:35 -0700

    Added parse_merchants and parser test

--- sales_engine.rb
+++ sales_engine.rb
@@ -1,8 +1,30 @@
-class SalesEngine
+require 'csv'
 
-  def initialize(string)
-    @string = string
+class SalesEngine
+  def initialize
+    @merchant_totals = []
   end
 
+  def startup
+    load_merchants
+    "Load complete!"
+  end
+
+  def load_merchants
+    merchant_file = CSV.open('./data/merchants.csv', headers: true, header_converters: :symbol)
+    parse_merchants(merchant_file)
+  end
+
+  def parse_merchants(merchant_file)
+    merchant_file.each do |merchant|
+      merchant  = {
+                id: merchant[:id],
+                name: merchant[:name],
+                created_at: merchant[:created_at],
+                updated_at: merchant[:updated_at],
+                   }
+      @merchant_totals << merchant
+    end
+  end
 
 end
--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -3,8 +3,19 @@ require './test/support'
 class SalesEngineTest < MiniTest::Unit::TestCase
   
   def test_it_exists
-    sales_engine = SalesEngine.new("parameter?")
+    sales_engine = SalesEngine.new
     assert_kind_of SalesEngine, sales_engine
+  end
+
+  def test_that_merchant_csv_loads_upon_startup
+    sales_engine = SalesEngine.new
+    assert_equal "Load complete!", sales_engine.startup
+  end
+
+  def test_that_merchant_csv_parsed_correctly
+    sales_engine = SalesEngine.new
+    sales_engine.startup
+    assert_equal 100, @merchant_totals.to_a
   end
 
 end
